d35560c7df02e1378b7d067baed223f6cc59b623
Edmond Zhou <19zhoue90@stu.smuhsd.org>
2018-10-09 11:43:46 -0700

    1st Commit

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -1,4 +1,3 @@
- 
 public class Quadratic {
 	public static double square(double num) {
 		double answer;
---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,9 @@
+import java.util.*;
+	public class QuadraticClient{		
+		public static void main(String args[]){
+	Scanner userInput = new Scanner(System.in);
+	
+	userInput.nextDouble();
+	
+		}
+	}
